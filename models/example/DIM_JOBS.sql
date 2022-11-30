99a7bf3dcf3e38b8d74291f95dd88c02b8db25a3
bala rohit nelluri <brohit@dfi.dc>
2022-11-30 13:41:01 +0530

    add your commit message here

--- models/example/DIM_JOBS.sql
+++ models/example/DIM_JOBS.sql
@@ -1,6 +1,9 @@
 {{ config(
     materialized='call_proc',
+    database='FIVETRAN_DATABASE',
+    schema = 'DBO',
+    pre_hook = "use schema {{ database }}.{{ schema }};"
     )
 }}
 
-call simple_stored_procedure_example();
+call "{{ database }}"."{{ schema }}".simple_stored_procedure_example();
